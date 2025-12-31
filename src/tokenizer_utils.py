2e236c13f2fd1534945754e8cc3dbd0b13097504
Roheet <roheetbasu@gmail.com>
2025-12-31 12:59:02 +0545

    completed train function

--- src/tokenizer_utils.py
+++ src/tokenizer_utils.py
@@ -33,7 +33,17 @@ class NepaliSentencePieceTokenizer:
         
     def load(self):
         
-        if not os.path.exists()
+        if not os.path.exists(self.model_file):
+            raise FileNotFoundError(
+                f"Tokenizer model not found from {self.model_file}"
+                "please train the tokenzier first"
+            )
+            
+        self.sp = spm.SentencePieceProcessor()
+        self.sp.load(self.model_file)
+        
+        print(f" Loaded tokenizer from {self.model_file}")
+        print(f" Vocabulary size: {self.vocab_size}")
         
     def train(self, data_paths: List[str], force_retrain: bool = False):
         
@@ -42,3 +52,52 @@ class NepaliSentencePieceTokenizer:
             print("Loading existing tokenzier.....")
             self.load()
             
+        print("Training tokenizer....")
+        
+        all_texts = []
+        
+        for data_path in data_paths:
+            if not os.path.exists(data_path):
+                print(f"Warning:{data_path} not found, skipping...")
+                continue
+            
+            df = pd.read_csv(data_path)
+            all_texts.extend(df.dropna().astype(str))
+            
+            if len(all_texts) == 0:
+                raise ValueError(f'No training data found')
+            
+        #save to temporary files
+        temp_file = f"{self.model_prefix}_temp_file.txt"
+            
+        with open(temp_file, 'w', encoding='utf-8') as f:
+            for text in all_texts:
+                f.write(text.strip() + '\n')
+                
+        print(f"Vocab_size:{self.vocab_size}")
+        print(f"Model_type:{self.model_type}")
+        print(f"Character_coverage:{self.character_coverage}")
+        
+        try:
+            spm.SentencePieceProcessor(
+                input = temp_file,
+                model_prefix = self.model_prefix,
+                character_coverage = self.character_coverage
+                pad_id = self.pad_id
+                unk_id = self.unk_id
+                bos_id = self.bos_id
+                eos_id = self.eos_id
+                user_defined_symbols = ['[PAD]','[UNK]','[BOS]','[EOS]'],
+                normalization_rule_name = 'nfkc'
+            )
+            
+            print(f"Tokenizer Trained Sucessfully.....")
+            print(f"model saved to: {self.model_file}")
+            print(f"Vocabulary saved to: {self.vocab_file}")
+            
+        finally:
+            #clean up temporary file
+            if os.path.exists(temp_file):
+                os.remove(temp_file)
+            
+        self.load()                     
